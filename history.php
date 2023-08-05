326e939b4e54438d1495f4203e2d020e5f2ab1a7
Anaeto Echezona <cechelon@gmail.com>
2023-08-05 02:20:16 -0700

    first commit

---
+++ history.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Welcome to James Buchanan Pub and Restaurant</title>
+<link rel="stylesheet" type="text/css" href="xres/css/style.css" />
+<link rel="icon" type="image/png" href="xres/images/favicon.png" />
+<link type="text/css" href="css/styles.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+    <h1><a href="index.php"><img src="images/a.png"  width="150" height="150" alt="James Buchanan Pub and Restaurant" /></a></h1>
+        <ul id="mainnav">
+			<li><a href="index.php">Home</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li class="current"><a href="history.php">History</a></li>
+            <li><a href="routes.php">Routes</a></li>
+            <li><a href="location.php"></a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+    	</ul>
+	</div>
+    <div id="content">
+    	<div id="gallerycontainer">
+			<div class="portfolio-area" style="margin:0 auto; padding:140px 20px 20px 20px; width:820px;">	
+					YSG Group of Companies is a wholly owned private Nigerian conglomerate comprising of viable subsidiaries in the Transport, Hospitality, Real Estate, Oil & Gas, Dredging & Quarry, and Rural Market Development Sectors.
+
+The Parent Company, Young Shall Grow Motors Transport Limited was found in 1972 by the Chairman, Chief Dr. Vincent Amaechi Obianodo MON (Member Order of the Niger), a business Mogul with a Midas touch. He is the Chairman of YSG Transport, Rockview hotels, YSG Dredging, Vineegas, Vineeoil, and YSG properties. He was the co-owner of Cellcom Telecoms, a Fixed CDMA Telecommunication Company which was later sold to Visafone in 2006. 
+
+His determination to provide cheap and comfortable road transportation made the Parent Company, Young Shall Grow Motors Limited, one of the biggest Private Transport Companies in Africa . Also,In a bid to provide mass employment in the rural areas, the Chairman has invested enormously in Rural Market Development by building and subletting shops and stores at affordable prices. One of such notable edifice is the ultra modern urban market in Onitsha located opposite the famous Onitsha main market in the Eastern part of Nigeria which is presently the biggest market in west Africa with about 3,000 shops, ware houses and banking hall,(land mass10 acres) managed by YSG properties Ltd , a real estate company as well as a rural and urban market development Company.
+
+
+					<br>
+					<br>
+					The Group have witnessed 21st Century transformation thanks to the innovations and foresightedness of the Managing Director Mr Vincent Obiora Obianodo, an Economist who holds a Msc in Service Management. He is the present Managing Director of YSG Transport Ltd, Rockview Hotels, YSG Dredging Ltd, YSG Properties Ltd, YSG Travellers’ Lodge Ltd, Vineegas Ltd, and Vinee Oil Ltd.
+
+Determined to reposition the Group in an info tech based economy, the managing Director established an Information Technology (IT) Company YSG Solutions in partnership with Bitla Soft and Telematis India to provide the needed IT Platform to boost services in the entire YSG group.
+
+He is the Chief Executive Officer and Managing Director of YSG Solutions Ltd, YSG Hubs Ltd, and YSG Entertainment ltd , a Media and entertainment Company established to help promote media values and help preserve our cultural heritage.He is also the co- owner of Copam Nigeria Ltd , a Motor and Spare Parts dealership company in partnership with Scania Brasil.
+
+With over 5,000 workers, the Group is present in 5 West African Countries and is desirous to spread its unique Services to more countries.
+
+At YSG Group we keep growing to meet dynamic needs of our customers, we await you in our world
+					<br>
+					
+					We are open daily from 10am to 12mn. SEE YOU THERE!
+					<br>
+				<div class="column-clear"></div>
+            </div>
+			<div class="clearfix"></div>
+        </div>
+    </div>
+    
+
+
+
+<div id="footer">
+	<h4>+234809 619 6617 &bull; <a href="contact-us.php">Festac, Opposite Odofun Local Government, 2nd Ave, Lagos, Nigeria.   </a></h4>
+	<p>Hours of Operation&nbsp;&nbsp;&bull;&nbsp;&nbsp;Mon - Sun: 10:00 am - 12:00 am</p>
+	<a href="index.php"><img src="images/a.png" width="50" height="50" alt="James Buchanan Pub and Restaurant" /></a>
+	<p>&copy; Copyright 2017 Young Shall Grow Bus | All Rights Reserved <br /></p>
+</div>
+
+</div>
+</body>
+</html>
